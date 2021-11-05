dfede72015ca397a056b3dc5f3c2086f48c84bb5
BJ McCotter <bjmccotter7192@gmail.com>
2021-11-05 15:42:40 -0500

    pushing api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :dogs, only: [:create, :index, :update, :destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :dogs, only: [:create, :index, :update, :destroy]
+    end
+  end  
 end
